677df6f9bac7f2bf8286b6170ab133aac6e0f771
Shahir Jalal <shahirjalalhazli@gmail.com>
2026-08-14 12:37:16 +0800

    remove dashboard and reports pages

--- frontend/src/app/shared/models/dashboard.model.ts
+++
@@ -1,9 +0,0 @@
-import { JobSummary } from './job.model';
-
-export interface DashboardSummary {
-  totalCollection: number;
-  totalCost: number;
-  totalProfit: number;
-  jobCount: number;
-  recentJobs: JobSummary[];
-}
